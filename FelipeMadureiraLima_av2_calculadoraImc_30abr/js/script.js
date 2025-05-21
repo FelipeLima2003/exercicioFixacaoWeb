e92af9dd8ce57b3b36444df483bd6ef1a640a14c
Felipe Lima <140981807+FelipeLima2003@users.noreply.github.com>
2025-05-21 16:43:10 -0300

    Add files via upload

--- FelipeMadureiraLima_av2_calculadoraImc_30abr/js/script.js
+++ FelipeMadureiraLima_av2_calculadoraImc_30abr/js/script.js
@@ -11,8 +11,7 @@ form.addEventListener('submit', function (e) {
 
   // Verifica se os valores são válidos
   if (peso <= 0 || altura <= 0) {
-    mensagem.textContent = "Por favor, insira um peso e altura válidos!";
-    mensagem.className = "mensagem mensagem-erro";
+    message(`Por favor insira um peso e altura correto`, 'error');
     return;
   }
 
@@ -20,17 +19,34 @@ form.addEventListener('submit', function (e) {
   const imc = peso / parseFloat((altura * altura));
 
   // Avalia o IMC
-  if (imc < 18.5) {
+    if (imc < 18.5) {
     mensagem.textContent = `O seu IMC é ${imc.toFixed(2)}. Você está abaixo do peso.`;
-    mensagem.className = "mensagem mensagem-sucesso";
+    mensagem.className = "mensagem mensagem-erro";
+    message(`Seu IMC é ${imc.toFixed(2)}. Você está abaixo do peso.`, 'error');
+
   } else if (imc >= 18.5 && imc <= 24.9) {
     mensagem.textContent = `O seu IMC é ${imc.toFixed(2)}. Você está com o peso normal.`;
     mensagem.className = "mensagem mensagem-sucesso";
+    message(`Seu IMC é ${imc.toFixed(2)}. Peso normal!`, 'success');
+
   } else if (imc >= 25 && imc <= 29.9) {
     mensagem.textContent = `O seu IMC é ${imc.toFixed(2)}. Você está com sobrepeso.`;
-    mensagem.className = "mensagem mensagem-sucesso";
+    mensagem.className = "mensagem mensagem-erro";
+    message(`Seu IMC é ${imc.toFixed(2)}. Sobrepeso.`, 'error');
+
   } else {
     mensagem.textContent = `O seu IMC é ${imc.toFixed(2)}. Você está obeso.`;
-    mensagem.className = "mensagem mensagem-sucesso";
+    mensagem.className = "mensagem mensagem-erro";
+    message(`Seu IMC é ${imc.toFixed(2)}. Obesidade.`, 'error');
   }
-});
+// ...
+function message(text, status = 'success') {
+    Toastify({
+        text: text,
+        duration: 2000,
+        style: {
+            background: status === 'success' ? '#84cc16' : '#dc2626',
+            boxShadow: 'none'
+        }
+    }).showToast();
+}})
